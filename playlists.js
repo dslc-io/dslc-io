b3dc3e816355ff28dc7fc31278385f3e3ff60610
Jon Harmon <jonthegeek@gmail.com>
2024-11-22 06:20:37 -0600

    Add anchors for books.
    Add anchors for books, and highlight the selected book (via URL).
    
    When a dslc.video url redirects to video.html, parse unknown targets into hash anchors.

--- playlists.js
+++ playlists.js
@@ -13,6 +13,11 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     buildAndDisplayBookGroups(sortedKeys, bookGroups, container);
 
+    // Delay calling highlightAnchorFromUrl until the next event loop to ensure elements are rendered
+    setTimeout(highlightAnchorFromUrl, 0);
+
+    // Add listener for hashchange to handle highlighting when the anchor changes
+    window.addEventListener('hashchange', highlightAnchorFromUrl);
   } catch (error) {
     console.error("Error processing TSV data or building the list:", error);
   }
@@ -31,10 +36,10 @@ const buildAndDisplayBookGroups = (sortedKeys, bookGroups, container) => {
   const fragment = document.createDocumentFragment(); // Create a DocumentFragment
 
   sortedKeys.forEach(book_title => {
-    const cohorts = bookGroups[book_title];
+    const { cohorts, book_code } = bookGroups[book_title];
     if (cohorts.length === 0) return; // Skip empty groups
 
-    const column = createBookGroupColumn(book_title);
+    const column = createBookGroupColumn(book_title, book_code);
     const listContainer = createBookGroupList(cohorts);
 
     column.appendChild(listContainer);
@@ -44,12 +49,13 @@ const buildAndDisplayBookGroups = (sortedKeys, bookGroups, container) => {
   container.appendChild(fragment); // Append the fragment to the container
 };
 
-const createBookGroupColumn = (book_title) => {
+const createBookGroupColumn = (book_title, book_code) => {
   const column = document.createElement('div');
   column.classList.add('book-group-column'); // Add CSS class for column styling
 
   const titleHeader = document.createElement('h3');
   titleHeader.textContent = book_title;
+  titleHeader.id = book_code; // Set the anchor ID to book_code
   column.appendChild(titleHeader);
 
   return column;
@@ -77,4 +83,22 @@ const createBookGroupListItem = (cohort_id, playlist_url, cohort) => {
   // Append to the list
   listItem.appendChild(link);
   return listItem;
+};
+
+const highlightAnchorFromUrl = () => {
+  // Remove existing highlights
+  const previouslyHighlighted = document.querySelector('.book-group-column > .highlighted');
+  if (previouslyHighlighted) {
+    previouslyHighlighted.classList.remove('highlighted');
+  }
+
+  // Highlight the new anchor element
+  const hash = window.location.hash.slice(1);
+  if (hash) {
+    const targetElement = document.getElementById(hash);
+    if (targetElement) {
+      targetElement.classList.add('highlighted');
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 };
