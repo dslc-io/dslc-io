9dfa1f96277ff4441800594aa460f712ccd61a95
Jon Harmon <jonthegeek@gmail.com>
2024-12-06 10:16:06 -0600

    Update redirect-404.js
    Worked locally but evidently not when deployed.

--- redirect-404.js
+++ redirect-404.js
@@ -7,7 +7,7 @@ async function handlePageNotFound() {
       return;
     }
 
-    handleRedirection(
+    await handleRedirection(
       target,
       "https://docs.google.com/spreadsheets/d/e/2PACX-1vQJVHVYt-8eeuR8iq0cxEz1uMuLY02AdFyuSeSECQSxdLbWV9PqIeVzz4Lh_Udm1nT92FDBLXDTWMqV/pub?gid=1314751843&single=true&output=tsv"
     );
