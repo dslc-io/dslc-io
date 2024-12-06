7177dedd3b262ef431280c4e62efd76cf35e7fc4
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-12-06 16:29:14 +0000

    Built site for gh-pages

--- redirect-404.js
+++ redirect-404.js
@@ -7,11 +7,15 @@ async function handlePageNotFound() {
       return;
     }
 
-    await handleRedirection(
+    const redirected = await handleRedirection(
       target,
       "https://docs.google.com/spreadsheets/d/e/2PACX-1vQJVHVYt-8eeuR8iq0cxEz1uMuLY02AdFyuSeSECQSxdLbWV9PqIeVzz4Lh_Udm1nT92FDBLXDTWMqV/pub?gid=1314751843&single=true&output=tsv"
     );
-    redirectToNotFound();
+
+    // Only redirect to notfound.html if no redirection occurred
+    if (!redirected) {
+      redirectToNotFound();
+    }
   } catch (error) {
     console.error("Error during redirection:", error);
     redirectToNotFound();
